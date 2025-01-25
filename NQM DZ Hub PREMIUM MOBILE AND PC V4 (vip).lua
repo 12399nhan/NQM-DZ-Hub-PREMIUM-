f525b8d2261dd4c8333a4722f611c886ba840b0a
12399nhan <nle226208@gmail.com>
2025-01-25 20:52:41 +0700

    Update NQM DZ Hub PREMIUM MOBILE AND PC V4 (vip).lua

--- NQM DZ Hub PREMIUM MOBILE AND PC V4 (vip).lua
+++ NQM DZ Hub PREMIUM MOBILE AND PC V4 (vip).lua
@@ -7,7 +7,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(530, 350),
     Acrylic = true,
-    Theme = "Rose",
+    Theme = "beach",
     MinimizeKey = Enum.KeyCode.End
 })
 local Tabs = {
